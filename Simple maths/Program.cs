6ef9f9da77c8eed24afb026766b33d629b9894d0
Louis Semerano <semeranolouis@leerling.sint-rembert.be>
2025-02-05 09:20:49 +0100

    Simple maths alle oefeningen zijn af van h2

--- Simple maths/Program.cs
+++ Simple maths/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int resultaat1 = -1 + 4 * 6;
+            Console.WriteLine(resultaat1);
+            int resultaat2 = (35 + 5) % 7;
+            Console.WriteLine(resultaat2);
+            Double resultaat3 = 14 + -4 * 6 / 11;
+            Console.WriteLine(resultaat3);
+            double resultaat4 = 2 + 15 / 6 * 1 - 7 % 2;
+            Console.WriteLine(resultaat4);
         }
     }
 }
